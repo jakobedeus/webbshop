10048955414aa4cff216acebd89fc68cbac3ed77
Jakob Edéus <jakob@edeus.se>
2019-05-01 11:18:24 +0200

    add comment to presentation spec

--- src/app/product-presentation/product-presentation.component.spec.ts
+++ src/app/product-presentation/product-presentation.component.spec.ts
@@ -2,22 +2,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductPresentationComponent } from './product-presentation.component';
 import { ShowProductsComponent } from '../show-products/show-products.component';
+import { HttpClientModule } from '@angular/common/http';
 import { IMovie } from '../interface/IMovies';
 import { Input, Component } from '@angular/core';
 
 
 describe('ProductPresentationComponent', () => {
-  // Create variable of our new TestHostComponent
   let testHostComponent: TestHostComponent;
   let testHostFixture: ComponentFixture<TestHostComponent>;
-
   // let component: ProductPresentationComponent;
   // let fixture: ComponentFixture<ProductPresentationComponent>;
   
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductPresentationComponent, ShowProductsComponent, TestHostComponent ]
+      declarations: [ ProductPresentationComponent, ShowProductsComponent, TestHostComponent ],
+      imports: [HttpClientModule]
     })
     .compileComponents();
   }));
