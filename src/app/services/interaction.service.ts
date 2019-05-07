9ddb7990ac9db1f432f41a57809849906639098b
Jakob Edéus <jakob@edeus.se>
2019-05-07 18:11:03 +0200

    working on interaction service

---
+++ src/app/services/interaction.service.ts
@@ -0,0 +1,18 @@
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class InteractionService {
+
+//   private messageSource = new BehaviorSubject<string>("Default message");
+//   currentMessage = this.messageSource.asObservable();
+
+//   constructor() { }
+
+//   changeMessage(message:string) {
+//     this.messageSource.next(message);
+
+//   }
+// }
