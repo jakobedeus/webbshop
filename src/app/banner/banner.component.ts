f0127fafffba7b4ec1cccf7ce021f8fc5dc767e0
Jakob Edéus <jakob@edeus.se>
2019-05-31 21:59:39 +0200

    add banner img and product img styling

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { IMovie } from '../interface/IMovies';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponent implements OnInit {
+
+  constructor(service: DataService) { 
+
+    service.getProductData().subscribe((productData) => { this.movies = productData;});
+  }
+
+  movies: IMovie[];
+
+
+
+  ngOnInit() {
+    console.log(this.movies);
+    var bannerMovieImg = this.movies[0].imageUrl;
+    
+  }
+
+}
